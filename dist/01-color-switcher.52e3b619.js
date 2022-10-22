23df38dc376b0d658647ea965e0988b013cd9b43
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-10-23 02:26:09 +0300

    Change task 1

--- dist/01-color-switcher.52e3b619.js
+++ dist/01-color-switcher.52e3b619.js
@@ -503,6 +503,24 @@ function hmrAcceptRun(bundle, id) {
 }
 
 },{}],"cYUEh":[function(require,module,exports) {
+const refs = {
+    startBtn: document.querySelector("button[data-start]"),
+    stopBtn: document.querySelector("button[data-stop]"),
+    bodyId: document.querySelector("body"),
+    timeId: null
+};
+const changeColorBtn = refs.startBtn.addEventListener("click", nextColor);
+timerId = setInterval(()=>{
+    nextColor();
+}, 1000);
+// const stopChangeBnt = refs.stopBtn.addEventListener('click', clearInterval) => 
+// clearInterval(timerId);
+function nextColor() {
+    refs.bodyId.style.background = `${getRandomHexColor()}`;
+}
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
 },{}]},["gzrwE","cYUEh"], "cYUEh", "parcelRequire7bc7")
 
--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,26 @@
+const refs = {
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+    bodyId: document.querySelector('body'),
+    timeId: null,
+};
 
+const changeColorBtn = refs.startBtn.addEventListener('click', nextColor) => 
+    timerId = setInterval(() => {
+        nextColor();
+      }, 1000);
+ 
+// const stopChangeBnt = refs.stopBtn.addEventListener('click', clearInterval) => 
+// clearInterval(timerId);
+
+    
+
+
+function nextColor() {
+refs.bodyId.style.background = `${getRandomHexColor()}`;
+};
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  }
