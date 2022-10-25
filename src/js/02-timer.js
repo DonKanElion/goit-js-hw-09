2410812b1f881a13b7a3e65b840ed0ec363122f1
Anton Palienko <95186395+DonKanElion@users.noreply.github.com>
2022-10-25 21:47:20 +0300

    load task 2

---
+++ src/js/02-timer.js
@@ -0,0 +1,27 @@
+import flatpickr from "flatpickr";
+import "flatpickr/dist/flatpickr.min.css";
+
+const refs = {
+    input: document.querySelector("#datetime-picker"),
+    startBtn: document.querySelector("button[data-start]"),
+    days: document.querySelector('span[data-days]'),
+    hours: document.querySelector('span[data-hours]'),
+    min: document.querySelector('span[data-minutes]'),
+    sec: document.querySelector('span[data-seconds]'),
+}
+
+const options = {
+    enableTime: true,
+    time_24hr: true,
+    defaultDate: new Date(),
+    minuteIncrement: 1,
+    onClose(selectedDates) {
+      console.log(selectedDates[0]);
+    },
+  };
+  
+
+flatpickr(refs.input, options); 
+
+
+
