558c6c19786b948b656349df5abd7e0573e42a21
DonKanElion <95186395+DonKanElion@users.noreply.github.com>
2022-11-01 21:51:20 +0000

    Deploying to gh-pages from @ DonKanElion/goit-js-hw-09@b03c76c7b69143cc5095c63bd464951999f1bf67 🚀

--- 03-promises.0b9b2fe5.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("input[name=delay]"),document.querySelector("input[name=step]"),document.querySelector("input[name=amount]");
-//# sourceMappingURL=03-promises.0b9b2fe5.js.map
--- 03-promises.ed2b2bfd.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("input[name=delay]"),document.querySelector("input[name=step]"),document.querySelector("input[name=amount]");
-//# sourceMappingURL=03-promises.ed2b2bfd.js.map
