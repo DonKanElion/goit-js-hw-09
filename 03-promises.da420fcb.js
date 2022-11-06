d5b6548fe87109a9e2f1ce850e6269e0aa3c5e7d
DonKanElion <95186395+DonKanElion@users.noreply.github.com>
2022-11-06 15:19:58 +0000

    Deploying to gh-pages from @ DonKanElion/goit-js-hw-09@8bfcfae2a7e131f92535fedf151baf4ad38b5546 🚀

--- 03-promises.da420fcb.js
+++
@@ -1,2 +0,0 @@
-document.querySelector("input[name=delay]"),document.querySelector("input[name=step]"),document.querySelector("input[name=amount]"),document.querySelector("button[type=submit]");(function(e,t){const o=Math.random()>.3;return new Promise(((e,t)=>{setTimeout((()=>{o&&e("Успіх!!!!!"),t("Все пропало це reject!!1")}),2e3)}))})().then((({position:e,delay:t})=>{console.log(`✅ Fulfilled promise ${e} in ${t}ms`)})).catch((({position:e,delay:t})=>{console.log(`❌ Rejected promise ${e} in ${t}ms`)}));
-//# sourceMappingURL=03-promises.da420fcb.js.map
